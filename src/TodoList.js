827e9ea282dfaa5932ea80009d7c9dd323a7b012
Bernard Strong <bstrong71@gmail.com>
2018-01-13 11:49:24 -0500

    added addItem event handler to run on submit of form; add items array to store items

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 export default class TodoList extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    // state includes items array to store various items
+    this.state = {
+      items: [];
+    };
+
+    this.addItem = this.addItem.bind(this);
+  }
+
+  // addItem event handler
+  addItem(e) {
+
+  }
+
   render() {
     return(
       <div className='todoListMain'>
         <div className='header'>
-          <form>
+          {/* listen for the submit event on the form and call the addItem method.
+             Used onSubmit because button type is set to submit */}
+          <form onSubmit={this.addItem}>
             <input placeholder='enter task'></input>
             <button type='submit'>add</button>
           </form>
